76e9488705538244b4b42d04159ec39a5b855923
Adrián Lozano Martínez <lozma.adrian@gmail.com>
2025-05-17 16:50:29 +0000

    finish silver

--- models/staging/sql_server_dbo/stg_sql_server_dbo__order_items.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__order_items.sql
@@ -15,25 +15,6 @@ WITH order_items AS (
         CAST(_fivetran_synced AS TIMESTAMP)::DATE AS _fivetran_synced_date,
         CAST(_fivetran_synced AS TIMESTAMP)::TIME AS _fivetran_synced_time
     FROM {{ source('sql_server_dbo', 'order_items') }}
-),
-
-products AS (
-    SELECT
-        product_id,
-        price
-    FROM {{ ref('stg_sql_server_dbo__products') }}
 )
 
-SELECT
-    oi.order_item_id,
-    oi.order_id,
-    p.product_id,
-    oi.quantity,
-    p.price,
-    oi._fivetran_deleted,
-    oi._fivetran_synced,
-    oi._fivetran_synced_date,
-    oi._fivetran_synced_time
-FROM order_items oi
-LEFT JOIN products p
-  ON oi.product_id = p.product_id
+SELECT * FROM order_items
